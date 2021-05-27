ec93474331155374c7ee1920c76243595fd1c550
OriginCode <self@origincode.me>
2021-05-27 15:25:05 +0800

    feature: optimise code

--- src/statistics/mod.rs
+++ src/statistics/mod.rs
@@ -1,12 +1,11 @@
 pub trait Data {
     fn get_mean(&self) -> f32;
     fn get_sd(&self) -> f32;
-}
-
-pub fn get_param_or_stat(data: impl Data, val: impl Into<f32>) -> f32 {
-    let mean = data.get_mean();
-    let sd = data.get_sd();
-    (val.into() - mean) / sd
+    fn get_param_or_stat(&self, val: impl Into<f32>) -> f32 {
+        let mean = self.get_mean();
+        let sd = self.get_sd();
+        (val.into() - mean) / sd
+    }
 }
 
 pub mod population;
